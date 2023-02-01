9869272ae5a5adf27a08cbd5d13cafe0c73f1cff
User <User@211-03>
2023-02-01 13:41:16 +0900

    초기화

---
+++ todolist/src/com/ktdsuniversity/edu/todo/service/TodoService.java
@@ -0,0 +1,46 @@
+package com.ktdsuniversity.edu.todo.service;
+import java.util.List;
+
+import com.ktdsuniversity.edu.todo.dao.TodoDAO;
+import com.ktdsuniversity.edu.todo.vo.TodoVO;
+public class TodoService {
+
+	//dao를 호출하려면 멤버변수를 가지고 있어야 함.
+	private TodoDAO todoDAO;
+	
+	public TodoService() {
+		todoDAO = new TodoDAO();
+		
+	}
+	//등록
+	public boolean create(String itemName) {
+		return todoDAO.create(itemName) > 0;
+	}
+	//조회(1건)
+	public TodoVO read(int key) {
+		return todoDAO.read(key);
+		
+	}
+	//조회(모든건)
+	public List<TodoVO> readAll(){
+		return todoDAO.readAll();		
+	}
+	
+	//수정
+	public boolean update(int key, boolean isSucess) {
+		return todoDAO.update(key, isSucess) > 0;
+	}
+	
+	
+	//삭제(1건)
+	public boolean delete(int key) {
+		return todoDAO.delete(key) > 0;
+	}
+	
+	//삭제(모든건)
+	public boolean deleteAll() {
+		return todoDAO.deleteAll() > 0;
+	}
+	
+	
+}
